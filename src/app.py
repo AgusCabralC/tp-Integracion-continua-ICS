e434c9551a4f19f6f2be953e6b0c49ff2d85f0aa
AgusCabralC <agustinrobertino@gmail.com>
2026-06-16 00:20:38 -0300

    agregado de interfaz y boton de consulta

--- src/app.py
+++ src/app.py
@@ -1,24 +1,25 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import datetime
 
 app = Flask(__name__)
 
+# NUEVA RUTA: Sirve la interfaz visual con el boton
+@app.route('/', methods=['GET'])
+def pagina_principal():
+    return render_template('index.html')
+
+# NUESTRO ENDPOINT: Procesa la logica de fondo (intacta para la IC)
 @app.route('/es-navidad', methods=['GET'])
 def consultar_navidad():
-    # 1. Si el usuario manda parámetros extraños en la URL, los rechazamos (Control de usuario erróneo)
     if request.args:
         return jsonify({"error": "Peticion invalida. No se admiten parametros."}), 400
         
-    # 2. Obtenemos la fecha actual del servidor
     hoy = datetime.datetime.now()
-    
-    # 3. Verificamos si es 25 de Diciembre (Mes 12, Día 25)
     if hoy.month == 12 and hoy.day == 25:
         es_navidad = True
     else:
         es_navidad = False
         
-    # 4. Devolvemos la respuesta en estructura JSON limpia
     return jsonify({"es_navidad": es_navidad}), 200
 
 if __name__ == '__main__':
