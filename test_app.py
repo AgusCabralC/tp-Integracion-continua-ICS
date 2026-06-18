a12713c811561ee00ddb60512b4edb50020ca6b8
AgusCabralC <agustinrobertino@gmail.com>
2026-06-17 21:10:04 -0300

    tarjeta 5: fuerzo un error en los tests a proposito

--- test_app.py
+++ test_app.py
@@ -10,7 +10,7 @@ class PruebasBotonNavidad(unittest.TestCase):
         self.navegador_prueba = app.test_client()
 
     def test_caso_comun_no_es_navidad(self):
-        """Filtro: Verifica que un día común devuelva False"""
+        # Verifica que un día común devuelva False
         fecha_falsa = datetime.datetime(2026, 6, 15)
         with patch('datetime.datetime') as mock_datetime:
             mock_datetime.now.return_value = fecha_falsa
@@ -23,7 +23,7 @@ class PruebasBotonNavidad(unittest.TestCase):
             self.assertFalse(datos["es_navidad"]) # Lógica de negocio
 
     def test_caso_borde_si_es_navidad(self):
-        """Filtro: Verifica que el 25/12 devuelva True obligatoriamente"""
+        #Verifica que el 25/12 devuelva True obligatoriamente
         fecha_navidad = datetime.datetime(2026, 12, 25)
         with patch('datetime.datetime') as mock_datetime:
             mock_datetime.now.return_value = fecha_navidad
@@ -32,9 +32,9 @@ class PruebasBotonNavidad(unittest.TestCase):
             datos = json.loads(respuesta.data)
             
             self.assertEqual(respuesta.status_code, 200)
-            self.assertTrue(datos["es_navidad"]) # Lógica de negocio
+            self.assertTrue(datos["es_navidad"]) 
 
     def test_rechazo_usuario_erroneo(self):
-        """Filtro: Comprueba que la app controle y rechace basura del usuario"""
+        #verifica que el usuario no escriba parametros en la URL, y que el sistema los rechace
         respuesta = self.navegador_prueba.get('/es-navidad?hack=123')
         self.assertEqual(respuesta.status_code, 400) # Valida el código de rechazo
